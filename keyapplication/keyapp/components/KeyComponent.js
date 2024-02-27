1b43a187eb550cf11420c2b838e9d1486ebb1381
kelvin <kelvinperrie@gmail.com>
2024-02-27 15:48:16 +1300

    getting full species info for previews

--- keyapplication/keyapp/components/KeyComponent.js
+++ keyapplication/keyapp/components/KeyComponent.js
@@ -4,7 +4,7 @@ import keyData from '../public/key.json';
 import speciesData from '../public/species.json';
 import React, { useState } from 'react';
 
-
+// draws breadcrumbs to show links to previous questions answered
 const HistoryComponent = (props) => {
 
     const { stepHistory, currentKeyStep, handle_breadcrumbClick } = props;
@@ -12,7 +12,7 @@ const HistoryComponent = (props) => {
     const currentStepJsx = <span className='breadrumb'>Q{currentKeyStep.id}</span>
 
     let historyOutput = "";
-    historyOutput = stepHistory.map((item, index) => (<a href="" className='breadrumb' onClick={((e) => handle_breadcrumbClick(item, e)) } key={item.id}>Q{item.id}</a>))
+    historyOutput = stepHistory.map((item, index) => (<span className='breadrumb' key={item.id}><a href="" onClick={((e) => handle_breadcrumbClick(item, e)) } >Q{item.id}</a></span>))
     historyOutput = <div className="breadrumbs">Breadcrumbs: {historyOutput}{currentStepJsx}</div>
 
     return(
@@ -23,13 +23,25 @@ const HistoryComponent = (props) => {
 
 }
 
+const SpeciesPreviewComponent = (props) => {
+    const { speciesData } = props;
+    return (
+        <div className="species-preview">
+            <img className="species-image" src={"images/"+speciesData.images[0].fileName} />
+            <div>{speciesData.scientificName}</div>
+        </div>
+    )
+}
+
 const FilteredSpeciesComponent = (props) => {
     const { filteredSpecies } = props;
     //console.log(filteredSpecies);
     return(
         <div>
             Possible species are:
-            {filteredSpecies.map((item) => (<div key={item}>{item}</div>))}
+            <div className="species-preview-container">
+                {filteredSpecies.map((item) => (<SpeciesPreviewComponent key={item.scientificName} speciesData={item} />))}
+            </div>
         </div>
     )
 }
@@ -52,20 +64,37 @@ function FindCurrentKeyStepById(idToSet) {
     return foundStep;
 }
 
+function GetSpeciesDataByScientificName(scientificName) {
+    //console.log("we're looking for " + scientificName)
+    let foundSpecies = speciesData.find((item) => item.scientificName === scientificName);
+    if(foundSpecies == null) {
+        console.log("this is probably a problem, we couldn't find the species data for the scientific name of " + scientificName)
+    }
+    return foundSpecies;
+}
+
 function GetFilteredSpeciesForStepAndChildren(currentKeyStepId) {
     // locate the current step in the key data
     // get any species it has
     // check any children for species they have
-    // put them in an array
+    // get the species data for the item
+    // put the species data in an array
     // return the array
 
     let currentStep = FindCurrentKeyStepById(currentKeyStepId);
     let foundSpecies = []; //currentStep.options.map((item) => (item.speciesToMoveTo)).filter(item => item);
     if(currentStep) {
         for(let i = 0; i < currentStep.options.length; i++) {
+            // if there are species to move to then collect information on them
             if(currentStep.options[i].speciesToMoveTo != null) {
-                foundSpecies.push(currentStep.options[i].speciesToMoveTo);
+                // this is the name of the species, we want to get the full data set for that name
+                let foundSpeciesName = currentStep.options[i].speciesToMoveTo;
+                let foundSpeciesData = GetSpeciesDataByScientificName(foundSpeciesName);
+                if(foundSpeciesData) {
+                    foundSpecies.push(foundSpeciesData);
+                }
             }
+            // if there are more steps to move to then go to them to see if there are any species within them
             if(currentStep.options[i].idToMoveTo != null) {
                 let results = GetFilteredSpeciesForStepAndChildren(currentStep.options[i].idToMoveTo)
                 foundSpecies = foundSpecies.concat(results);
